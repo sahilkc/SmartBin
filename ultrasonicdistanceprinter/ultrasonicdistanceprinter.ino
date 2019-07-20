7bbfd0bc44203a31aedc6ca2f87f8d3dfe651239
Sahil <sahilkc39@gmail.com>
2019-07-20 20:37:45 +0545

    distance by light testing

--- ultrasonicdistanceprinter/ultrasonicdistanceprinter.ino
+++ ultrasonicdistanceprinter/ultrasonicdistanceprinter.ino
@@ -1,5 +1,7 @@
 #define NOTIFYDISTANCE 10
 int tme=0;
+int ledPin1=8;
+int ledPin2=9
 const int echopin=3;
 const int trigerpin=2;
 int distance=0;
@@ -8,6 +10,8 @@ unsigned long lastfulltime=0;
 bool firsttime=1;
 void setup() {
   pinMode(3,INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
   pinMode(2,OUTPUT);
   Serial.begin(9600);
 }
@@ -23,5 +27,18 @@ tme=pulseIn(echopin,HIGH);
 //time in microseconds
 
 distance=tme*0.017;
-Serial.println(distance);
+if(distance<10)
+{
+  digitalWrite(ledPin1,HIGH);
+  delay(100);
+  digitalWrite(ledPin1,LOW);
+  delay(100);
+  }
+ else
+ { digitalWrite(ledPin2,HIGH);
+  delay(100);
+  digitalWrite(ledPin2,LOW);
+  delay(100);
+  }
+//Serial.println(distance);
 }
